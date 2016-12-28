b9014780b490bb37130dad9740c6c6a926f16b3b
tomfriwel <976932447@qq.com>
2016-12-28 10:15:54 +0800

    Initial Commit

---
+++ TestAnimator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAnimator
+//
+//  Created by tomfriwel on 28/12/2016.
+//  Copyright © 2016 tomfriwel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestAnimator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestAnimator
+//
+//  Created by tomfriwel on 28/12/2016.
+//  Copyright © 2016 tomfriwel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
